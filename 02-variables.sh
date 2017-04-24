786820fb4f2fd59939644742e489d2d31bf03722
Jean-Simon Beaudet <jsimon.b93@gmail.com>
2017-04-24 17:41:18 -0400

    Ajout des notes 02-variables.sh

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+#En BASH, il ne faut pas mettre d'espace entre les signes !!
+prenom=John
+echo $prenom
+
+declare -i age=35 #Créer un int
+declare -r age2=33 #Devient en lecture seule
+
+# On met des guillements lorsqu'il y a des espaces
+cours="Cours de Linux"
+
+# L'ordre des paramètres envoyés en démarrant lexécutable
+echo Le programme $0 a pris le paramètre $1
