717eeab8ea0e70778f419de209fe55569f4c03bc
Jean-Simon Beaudet <jsimon.b93@gmail.com>
2017-04-26 15:11:18 -0400

    Ajout des notes 08-regexp.sh

--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+echo -n "Lettres:"
+read lettres
+
+# read -p "Lettre:" lettres
+
+if [[ $lettres =~ ^[0-9]+$ ]]
+then
+	echo "C'est un nombre"
+fi
+
+if [[ $lettres =~ ^[a-zA-Z]+$ ]]
+then
+	echo "C'est un mot"
+fi
+
