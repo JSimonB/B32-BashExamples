8c9ed043e96081f662237d75d3bcb29db87c0bbd
Jean-Simon Beaudet <jsimon.b93@gmail.com>
2017-04-26 14:55:57 -0400

    Ajout des notes de 05-boucles.sh

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 
+for ((i=0;i<5;i++))
+do
+	echo $i
+done
+
+resultat=o
+
+while test $resultat = "o"
+do
+	read -p "Entrer une lettre : " resultat
+done
+
+maListe=`ls`
+
+for element in $maListe
+do
+	echo $element
+done
+
+
