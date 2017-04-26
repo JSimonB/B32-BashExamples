d6fc5c603b8def28a512db72d28ec2f0185c6b73
Jean-Simon Beaudet <jsimon.b93@gmail.com>
2017-04-26 16:28:32 -0400

    Exercice 3 terminé

--- Exercice3.sh
+++ Exercice3.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+FaireEspace(){
+
+	for ((i=0;i<$1;i++))
+	do
+		echo -n " "
+	done
+}
+
+for ((i=0;i<30;i++))
+do
+	echo -n "@"
+	sleep 0.1
+	clear
+	FaireEspace $i
+done
+echo "BOOM!"
+sleep 1
+
+
